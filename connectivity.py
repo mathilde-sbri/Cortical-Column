3952caa19fb66fd4e98cedb97053e02d9323f9da
Mathilde Proust <athildeproust01@gmail.com>
2026-02-23 16:56:52 +0100

    save

--- connectivity.py
+++ connectivity.py
@@ -6,9 +6,13 @@ import matplotlib.pyplot as plt
 conn_df = pd.read_csv('config/connection_probabilities2.csv', index_col=0)
 mask = conn_df == 0
 plt.figure(figsize=(7, 7))
-sns.heatmap(conn_df, annot=True, fmt='.3f', cmap='Reds', square=True,
+ax = sns.heatmap(conn_df, annot=True, fmt='.3f', cmap='Reds', square=True,
             linewidths=0.3, cbar_kws={'label': 'Connection Probability2'}, mask=mask, annot_kws={'size': 6}
             )
+
+for pos in [4, 8, 12, 16]:
+    ax.axhline(y=pos, color='black', linewidth=1)
+    ax.axvline(x=pos, color='black', linewidth=1)
 
 plt.title('V1 Connectivity Matrix')
 plt.xlabel('Postsynaptic Population')
@@ -20,9 +24,13 @@ plt.tight_layout()
 conn_df = pd.read_csv('config/conductances_AMPA2_alpha_v2.csv', index_col=0)
 mask = conn_df == 0
 plt.figure(figsize=(7, 7))
-sns.heatmap(conn_df, annot=True, fmt='.3f', cmap='Reds', square=True,
+ax = sns.heatmap(conn_df, annot=True, fmt='.3f', cmap='Reds', square=True,
             linewidths=0.3, cbar_kws={'label': 'Conductance values'}, mask=mask, annot_kws={'size': 6}
             )
+
+for pos in [4, 8, 12, 16]:
+    ax.axhline(y=pos, color='black', linewidth=1)
+    ax.axvline(x=pos, color='black', linewidth=1)
 
 plt.title('V1 conductances_AMPA  Matrix')
 plt.xlabel('Postsynaptic Population')
@@ -32,9 +40,13 @@ plt.tight_layout()
 conn_df = pd.read_csv('config/conductances_NMDA2_alpha_v2.csv', index_col=0)
 mask = conn_df == 0
 plt.figure(figsize=(7, 7))
-sns.heatmap(conn_df, annot=True, fmt='.3f', cmap='Reds', square=True,
+ax = sns.heatmap(conn_df, annot=True, fmt='.3f', cmap='Reds', square=True,
             linewidths=0.3, cbar_kws={'label': 'Conductance values'}, mask=mask, annot_kws={'size': 6}
             )
+
+for pos in [4, 8, 12, 16]:
+    ax.axhline(y=pos, color='black', linewidth=1)
+    ax.axvline(x=pos, color='black', linewidth=1)
 
 plt.title('V1 conductances_NMDA Matrix')
 plt.xlabel('Postsynaptic Population')
